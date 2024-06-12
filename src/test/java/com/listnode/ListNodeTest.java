d0d3d0c863bd213c7dd45b5a94b737f6b2571ac6
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2024-06-13 05:03:36 +0530

    ListNode first

---
+++ src/test/java/com/listnode/ListNodeTest.java
@@ -0,0 +1,13 @@
+package com.listnode;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ListNodeTest {
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
