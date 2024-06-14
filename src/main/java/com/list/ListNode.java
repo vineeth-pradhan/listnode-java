95076e481f6db2f99615ddd58732511700774c02
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2024-06-14 05:30:24 +0530

    Making constructors public

--- src/main/java/com/list/ListNode.java
+++ src/main/java/com/list/ListNode.java
@@ -1,11 +1,11 @@
 package com.list;
 
-class ListNode {
+public class ListNode {
     int val;
     ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    public ListNode() {}
+    public ListNode(int val) { this.val = val; }
+    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 
     public ListNode copyNode(){
         ListNode node = this;
